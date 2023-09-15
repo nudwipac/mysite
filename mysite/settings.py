ce36fe3b698012f2c456e33f97dcb305ca392562
Nudwipa <johndoe@example.com>
2023-09-15 16:43:43 +0700

    commit 6

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!-0!o3g06zd=+pd*sh5=*!+awnhnmh%d2!09qdka-abc1fuyk^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,10 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
+#STATIC_URL = 'static/'
+#STATICFILES_DIRS=[
+#    os.path.join(BASE_DIR,'static')
+#]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
